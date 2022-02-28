c51bb17d8a7e1410a9d64977d891867665e1b8f0
veniamintul <tulupovvenya@yandex.ru>
2022-02-28 03:08:21 +0300

    Add files via upload

--- js/time.js
+++ js/time.js
@@ -1,27 +1,13 @@
-let timerInput = document.getElementById("time"); // Берём строку
-let buttonRun = document.getElementById("button__time");// Берём кнопку запуска
-let timerShow = document.getElementById("timer"); // Берём блок для показа времени
+let time=1800;
+const countDownEl=document.querySelector(".countdown");
 
+setInterval(updateCountdown,1000);
 
-buttonRun.addEventListener('click', function() {
-    timeMinut = parseInt(timerInput.value) * 60
-})
-
-
-timer = setInterval(function () {
-    seconds = timeMinut%60 // Получаем секунды
-    minutes = timeMinut/60%60 // Получаем минуты
-    // Условие если время закончилось то...
-    if (timeMinut <= 0) {
-        // Таймер удаляется
-        clearInterval(timer);
-        // Выводит сообщение что время закончилось
-        alert("Время закончилось");
-    } else { // Иначе
-        // Создаём строку с выводом времени
-        let strTimer = `${Math.trunc(hour)}:${Math.trunc(minuts)}:${seconds}`;
-        // Выводим строку в блок для показа таймера
-        timerShow.innerHTML = strTimer;
-    }
-    --timeMinut; // Уменьшаем таймер
-}, 1000)
+function updateCountdown(){
+    const minutes=Math.floor(time/60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds:
+    seconds;
+    countDownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+}
---
+++ js/zacaz.js
@@ -0,0 +1,22 @@
+const phone = document.querySelector('#phone');
+phone.addEventListener("keydown",function(event){
+  let isDigit=false;   // нажали ли мы цифру .По умолчанию всегда false
+  if(event.key>=0||event.key<=9 ||event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace')
+  {
+      isDigit=true;
+  }
+  else{
+      event.preventDefault();
+  }
+
+  let isDash=false;   
+  if(event.key=="-"){
+    isDash=true; 
+  }
+
+  if(isDigit=false &&!isDash){    
+    event.preventDefault();
+}
+
+
+});
